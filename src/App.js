b51f396fe138a763f14909251e4d33ca6fd80442
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2023-05-06 11:21:51 -0400

    fetching data tripadvisor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import './App.css';
 import Header from './components/Header';
 import Card from './components/Card';
 import data from './data';
+import { useEffect } from 'react';
 function App() {
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      mode: 'no-cors',
+      // Referer: 'https://futuretravels01.com/',
+    };
+
+    fetch(
+      `https://api.content.tripadvisor.com/api/v1/location/nearby_search?latLong=42.3455%252C-71.1076&key=${process.env.REACT_APP_TRIP_ADVISOR_KEY}&language=en`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  }, []);
+
   const travels = data.map((item) => {
     return (
       <Card
@@ -16,7 +33,7 @@ function App() {
     );
   });
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
       {travels}
     </div>
